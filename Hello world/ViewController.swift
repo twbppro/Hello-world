8fd55c886593c838172673a615db787f3d7cd019
Jay Chang <jay10131991@gmail.com>
2018-01-12 13:47:38 +0800

    Initial Commit

---
+++ Hello world/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hello world
+//
+//  Created by Yawen Chang on 12/1/18.
+//  Copyright © 2018 Yawen Chang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
